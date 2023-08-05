dc9ae2e35e447620165f6f36f4a76507e5de402f
Arianna <95667502+aadams504@users.noreply.github.com>
2023-08-04 23:53:23 -0700

    made window component draggable

--- src/Window/Window.jsx
+++ src/Window/Window.jsx
@@ -1,11 +1,22 @@
+// import { Resizable } from "re-resizable";
+import Draggable from "react-draggable";
 import WindowImg from "../assets/mac-window.png";
 import "./Window.scss";
 
 const Window = () => {
+  // const [state, setState] = useState({ width: 320, height: 200 });
   return (
-    <div className="window-container">
-      <img className="window" src={WindowImg} />
-    </div>
+    <Draggable>
+      <div className="window-container">
+        <div className="window">
+          <div className="window-topbar">
+            <span className="close"></span>
+            <span className="minimize"></span>
+            <span className="expand"></span>
+          </div>
+        </div>
+      </div>
+    </Draggable>
   );
 };
 
